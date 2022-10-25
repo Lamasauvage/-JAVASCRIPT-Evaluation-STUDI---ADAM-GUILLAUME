bf966114bc36b79674f4eb8a08d6eb60662263cc
Guillaume <adam.guillaume78@gmail.com>
2022-10-25 13:16:58 +0200

    fonction welcome back

---
+++ Welcome back/index.js
@@ -0,0 +1,9 @@
+let welcomeEl = document.getElementById("welcome-el")
+
+let firstName = "Guillaume"
+let greeting = "Welcome back "
+
+welcomeEl.innerText = greeting + firstName 
+
+
+welcomeEl.innerText += "😜"
