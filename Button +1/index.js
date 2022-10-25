d2eb64a6c931f2c069331c1cdfa1830320b093c4
Guillaume <adam.guillaume78@gmail.com>
2022-10-25 12:17:40 +0200

    Ajout fonction save

--- Button +1/index.js
+++ Button +1/index.js
@@ -6,12 +6,9 @@ let count = 0
 
 function increment () {
   count = count + 1
-  countEl.innerText = count
-  console.log(count)
+  countEl.innerText = count 
 }
 
 function save() {
   console.log(count)
 }
-
-save()
