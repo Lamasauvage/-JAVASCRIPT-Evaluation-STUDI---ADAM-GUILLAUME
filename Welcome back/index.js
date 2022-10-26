8da41ba5e72f72c10b185cf1d4c6e65d1f8d1b64
Guillaume ADAM <104031542+Lamasauvage@users.noreply.github.com>
2022-10-26 15:08:11 +0200

    Delete Welcome back directory

--- Welcome back/index.js
+++
@@ -1,9 +0,0 @@
-let welcomeEl = document.getElementById("welcome-el")
-
-let firstName = "Guillaume"
-let greeting = "Welcome back "
-
-welcomeEl.innerText = greeting + firstName 
-
-
-welcomeEl.innerText += "😜"
