f2d06bf1d10aa1420d81142a72ff4c9ea2be9656
Guillaume <adam.guillaume78@gmail.com>
2022-10-28 17:57:31 +0200

    Finsalisation

--- Blackjack/index.js
+++ Blackjack/index.js
@@ -1,3 +1,8 @@
+let player = {
+  name: "Guillaume",
+  chips: 200,
+}
+
 let cards = []
 let sum = 0
 let hasBlackJack = false
@@ -6,11 +11,6 @@ let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
-
-let player = {
-  name: "Per",
-  chips: 145
-}
 
 let playerEl = document.getElementById("player-el")
 playerEl.textContent = player.name + " : " + player.chips + "€" 
