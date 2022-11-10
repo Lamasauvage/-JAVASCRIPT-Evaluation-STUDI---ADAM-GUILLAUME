744a6e22b36dacd5a35e973f2addaeed29b8447b
Guillaume <adam.guillaume78@gmail.com>
2022-11-10 17:19:55 +0100

    Console log + fonction randomNumber

--- Evaluation/index.js
+++ Evaluation/index.js
@@ -1,8 +1,14 @@
-function rollDice() {
+// let saveEl = document.getElementById(global-count)
+// let countEl = document.getElementById(count-el)
+// let count = 0 
+
+function getRandomDiceNumber() {
   let randomNumber = Math.floor( Math.random()*6 ) + 1
   if (randomNumber === 1) {
+    console.log(0)
       return 0 // 0 = perte de tous les points 
   } else {
+    console.log(randomNumber)
       return randomNumber 
   } 
 }
