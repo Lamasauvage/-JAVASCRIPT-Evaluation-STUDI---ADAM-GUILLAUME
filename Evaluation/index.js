d776ec4102daabb58378fcb92c039caab4fdb04d
Guillaume ADAM <adam.guillaume78@gmail.com>
2022-11-11 21:29:10 +0100

    Round function in progress

--- Evaluation/index.js
+++ Evaluation/index.js
@@ -1,11 +1,15 @@
+let round = 0
 let rollDiceResult = document.getElementById("roll-dice-result")
+let roundPlayerOne = document.getElementById("round-player-one")
+let messageEl = document.getElementById("message-el")
+
 
 // Generate random number
 
 function getRandomDiceNumber() {
   let randomNumber = Math.floor( Math.random()*6 ) + 1
   if (randomNumber === 1) {
-      return 0 // 0 = perte de tous les points
+      return 1 // 1 = perte de tous les points
   } else {
       return randomNumber
   }
@@ -24,4 +28,20 @@ function rollDice() {
 
 function renderGame() {
   rollDiceResult.innerHTML = roll
+
+  round += roll
+  roundPlayerOne.textContent = round
+
+
+  if (roll === 1) {
+    message = "You lost all your points, better luck next time"
+  } else {
+    message = ""
+  }
+  messageEl.textContent = message
 }
+
+// Ajouter le result du roll dice au current du player1 / Prendre le résultat du roll et l'ajouter au round player 1
+// Si roll = 1 / round over + roundnumber = 0
+
+
