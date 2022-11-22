c1e0112cb667066d142c07ad87d22c30ae3feb26
Guillaume ADAM <adam.guillaume78@gmail.com>
2022-11-22 14:24:17 +0100

    fixed reset function

--- Evaluation/index.js
+++ Evaluation/index.js
@@ -23,9 +23,27 @@ totalPlayer1.textContent = 0
 let winMessageEl
 let round = 0;
 let activePlayer = 0;
-const scores = [0,0];
+let scores = [0,0];
+
+// Hide dice
 
 diceDisplay.classList.add("hidden")
+
+// Function New Game = reset all score to 0
+
+function newGame() {
+  diceDisplay.classList.add("hidden")
+  totalPlayer0.textContent = 0
+  totalPlayer1.textContent = 0
+  roundPlayer0.textContent = 0
+  roundPlayer1.textContent = 0
+  rollDiceResult.innerHTML = 0
+  round = 0;
+  activePlayer = 0;
+  scores = [0,0];
+}
+
+newGame();
 
 // Generate random number
 
@@ -48,7 +66,7 @@ function switchPlayer() {
 
     activePlayer = activePlayer === 0 ? 1 : 0
     round = 0
-    // CSS
+
     player0El.classList.toggle("player--active")
     player1El.classList.toggle("player--active")
 }
@@ -70,16 +88,6 @@ function renderGame() {
   } else {
     switchPlayer()
   }
-
-
-// Message en cas de roll 1 (Temporaire)
-
-  if (rollNumber === 1) {
-    message = "You lost all your points"
-  } else {
-    message = ""
-  }
-  messageEl.textContent = message
 }
 
 
@@ -103,17 +111,17 @@ function hold() {
 }
 
 
-// Function New Game = reset all score to 0
+ // I wanted to dynamicaly render the player who won but i don't know how to correcly write it, i have the error msg "undefinedplayer" when score <=100)" /
 
- function newGame() {
-  totalPlayer0.textContent = 0
-  totalPlayer1.textContent = 0
-  roundPlayer0.textContent = 0
-  roundPlayer1.textContent = 0
-  rollDiceResult.innerHTML = 0
-  round = 0;
-  activePlayer = 0;
-  scores = [0,0];
-  diceDisplay.classList.add("hidden")
+    // winMessageEl = `player--${activePlayer}` 
+    // winMessage.textContent = winMessageEl + " WIN "
 
-}
+    // or 
+
+    //document.getElementById(`name--${activePlayer}`).textContent = winMessageEl
+    //winMessage.textContent = winMessageEl + " WIN "
+
+
+
+
+
